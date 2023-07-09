c92bcfbc2a93cc8c84291203987a0cccdb9ae537
Kachielite <40024356+Kachielite@users.noreply.github.com>
2023-07-09 21:38:10 +0100

    added tailwind, eslint, prettier and husky

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-2xl text-red-400">This is the first page of an awesome</h1>
+    </div>
+  );
+}
+
+export default App;
